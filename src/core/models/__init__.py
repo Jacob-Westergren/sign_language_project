32580ca9dfe3841f0ff22b86d999bc2dd7112ff3
JacobWestergren3 <jacob.westergren.student@gmail.com>
2025-02-08 22:46:37 +0100

    refactor: Changed name of models folder to avoid confusion with ML models.

--- src/core/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Data models package initialization
-# Define what is imported when the package "models" is imported
-from .scene import Scene, SceneData
-from .person import DetectedPerson
---
+++ src/core/structures/__init__.py
@@ -0,0 +1,4 @@
+# Data structures package initialization
+# Defines what is imported when the package "structures" is imported
+from .scene import Scene, SceneData
+from .person import DetectedPerson
